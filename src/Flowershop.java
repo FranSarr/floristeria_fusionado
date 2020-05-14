a3ae4b9c5b268fbf60da47355da011543a7d8df1
fsarrias <fsarrias@kolab.es>
2020-05-14 11:20:27 +0200

    Subo el código fusuinado con Roque, falta hacer la función stock

--- src/Flowershop.java
+++ src/Flowershop.java
@@ -3,9 +3,14 @@ public class Flowershop {
     private String flowershop_name;
     private ProductRepository repository;
 
+
     public Flowershop(String flowershop_name, ProductRepository repository) {
         this.flowershop_name = flowershop_name;
         this.repository = repository;
+    }
+
+    public void setFlowershop_name(String flowershop_name) {
+        this.flowershop_name = flowershop_name;
     }
 
     public void addFlower(String name, Double price, String colour) throws Exception{
